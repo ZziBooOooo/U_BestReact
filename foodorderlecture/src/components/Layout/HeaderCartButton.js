8d86b5e04e5573dc897a67593bf163bf4f408e7f
minsuh Jo <zziboo3773@gmail.com>
2023-07-14 01:55:23 +0900

    feat: FoodOrderApp lecture code complete

--- foodorderlecture/src/components/Layout/HeaderCartButton.js
+++ foodorderlecture/src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 import CartIcon from "./CartIcon";
 import CartContext from "../../store/cart-context";
@@ -6,15 +6,40 @@ import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
   const cartCtx = useContext(CartContext);
+  const { items } = cartCtx;
 
   // reduce 메서드 -> 누적계산 https://tocomo.tistory.com/26 참고
   const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
     return curNumber + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${
+    btnIsHighlighted ? classes.bump : ""
+  }`;
+
+  //사이드이펙트
+  useEffect(() => {
+    if (cartCtx.items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    // useEffect에서 리턴함수 -> 리액트에 의해 자동으로 호출
+    // 버튼을 빠르게 클릭했을때, 전의 타이머는 지우고
+    // 다음타이머를 호출할 수  있음
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
--- foodorderlecture/src/store/CartProvider.js
+++ foodorderlecture/src/store/CartProvider.js
@@ -9,10 +9,62 @@ const defaultCartState = {
 
 const cartReducer = (state, action) => {
   if (action.type === "ADD") {
-    // concat은 새 배열 반환 - 기존메모리에 저장된 데이터 변형 X
-    const updatedItems = state.items.concat(action.item);
     const updatedTotalAmount =
       state.totalAmount + action.item.price * action.item.amount;
+
+    const existingCartItemsIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingCartItemsIndex];
+
+    let updatedItems;
+
+    // 데이터가 이미 존재하는 경우
+    if (existingCartItem) {
+      const updatedItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      // 기존배열을 건드리지 않고 새 배열을 만든다.
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemsIndex] = updatedItem;
+    } else {
+      // concat은 새 배열 반환 - 기존메모리에 저장된 데이터 변형 X
+      updatedItems = state.items.concat(action.item);
+    }
+
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    };
+  }
+  if (action.type === "REMOVE") {
+    // const updatedTotalAmount = state.totalAmount;
+    // const updatedItems = state.items.map((item) => {
+    //   if (item.id === action.id) {
+    //     item.amount = item.amount - 1;
+    //   }
+    // });
+    // console.log(updatedItems);
+    // return {
+    //   items: updatedItems,
+    //   totalAmount: updatedTotalAmount,
+    // };
+
+    const existingCartItemsIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingItem = state.items[existingCartItemsIndex];
+    const updatedTotalAmount = state.totalAmount - existingItem.price;
+    let updatedItems;
+
+    if (existingItem.amount === 1) {
+      updatedItems = state.items.filter((item) => item.id !== action.id);
+    } else {
+      const updatedItem = { ...existingItem, amount: existingItem.amount - 1 };
+      updatedItems = [...state.items];
+      updatedItems[existingCartItemsIndex] = updatedItem;
+    }
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount,
