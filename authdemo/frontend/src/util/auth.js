1aee95afb316c6aeeadca5aae2bed6a12939b350
minsuh Jo <zziboo3773@gmail.com>
2023-10-03 03:22:51 +0900

    FEAT : Using Token

--- authdemo/frontend/src/util/auth.js
+++ authdemo/frontend/src/util/auth.js
@@ -1,4 +1,22 @@
+import { redirect } from "react-router-dom";
+
 export function getAuthToken() {
   const token = localStorage.getItem("token");
   return token;
+}
+
+export function tokenLoader() {
+  return getAuthToken();
+}
+
+export function checkAuthLoader() {
+  const token = getAuthToken();
+
+  if (!token) {
+    // ?mode=login으로 가야하는거 아닌가?
+    return redirect("/auth");
+  }
+
+  return null;
+  // or Error을 throw해 에러페이지로
 }
