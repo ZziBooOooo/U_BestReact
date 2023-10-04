e55257b7f5ce18b584939d7e314d1e222c77d9e4
minsuh Jo <zziboo3773@gmail.com>
2023-10-04 20:11:34 +0900

    FEAT : Complete Router Demo App

--- authdemo/frontend/src/pages/Root.js
+++ authdemo/frontend/src/pages/Root.js
@@ -7,20 +7,32 @@ import {
 
 import MainNavigation from "../components/MainNavigation";
 import { useEffect } from "react";
+import { getTokenDuration } from "../util/auth";
 
 function RootLayout() {
   // const navigation = useNavigation();
+
+  // auth.js에서 getAuthToken의 리턴값에 따라 달라짐
   const token = useLoaderData();
   const submit = useSubmit();
 
+  // 의존성배열에 token -> token이 변경되면 다시 useEffect 실행해서
+  // token 없어진건지 expired 된건지 확인 후 알맞는 코드 실행
   useEffect(() => {
     if (!token) {
       return;
     }
 
+    if (token === "EXPIRED") {
+      submit(null, { action: "/logout", method: "post" });
+      return;
+    }
+
+    const tokenDuration = getTokenDuration();
+
     setTimeout(() => {
       submit(null, { action: "/logout", method: "post" });
-    }, 1 * 60 * 60 * 1000);
+    }, tokenDuration);
   }, [token, submit]);
 
   return (
